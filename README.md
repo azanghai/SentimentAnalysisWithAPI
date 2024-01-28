f81511693973cc561411362afab541569001cb53
azanghai <x5058226@qq.com>
2024-01-28 17:10:22 +0000

    update BaiduAPI

--- README.md
+++ README.md
@@ -1,37 +1,54 @@
-# 使用阿里云服务进行情感分析
+# 使用API服务进行情感分析
 
-该项目使用阿里云的[情感分析服务](https://help.aliyun.com/document_detail/179345.html)
-进行情感分析。相较于自行撰写代码使用API进行情感分析，该脚本提供了一个开箱即用（进行简单配置），进行批量情感分析的工具。
+目前，该脚本可以使用[阿里云]((https://help.aliyun.com/document_detail/179345.html))与[百度开放平台](https://ai.baidu.com/tech/nlp_apply/sentiment_classify)进行批量或单独的情感分析。相较于自行撰写代码使用API进行情感分析，该脚本提供了一个开箱即用（进行简单配置），进行批量情感分析的工具。
 
 ## 在开始之前
 
-首先，您需要获得使用阿里云服务的App Key和App Secret 。 如果您是杨老师课题组的成员或杨老师的学生。请您联络组会群中头像为一只大象在白色背景中的成员，或者直接联络杨老师获取配置的App
+首先，您需要获得使用阿里云服务的App Key和App Secret或百度云的App Key和App Secret，这取决于您想使用哪一种情感分析服务。**_好消息是_**如果您是*杨老师课题组*的成员或*杨老师*的学生。请您联络组会群中头像为一只大象在白色背景中的成员，或者直接联络杨老师获取配置的App
 Key与App Secret。
 
-在运行之前，请找到脚本（Scripts文件夹下的main.py文件），并修改下面的内容：
+### 安装运行脚本所需要的库
 
-如果您没有将App Key和App Secret添加至环境变量，则将下面的内容：
-
-```python
-access_key_id = os.environ['NLP_AK_ENV']
-access_key_secret = os.environ['NLP_SK_ENV']
-```
-
-修改为：
-
-```python
-access_key_id = '这里替换为你获得的AppKey'
-access_key_secret = '这里替换为你获得的AppSecret'
-```
-
-**请注意** 请不要将AppKey或AppSecret分享给其他任何人或上传至网络
-
-其次，您需要正确安装运行脚本所需的包。如果您使用的是Pycharm，则您在加载本项目时应当会有提示进行自动安装，否则，您需要手动安装，您可以在终端中分别运行下面的代码来安装：
+您需要正确安装运行脚本所需的包。如果您使用的是Pycharm，则您在加载本项目时应当会有提示进行自动安装，否则，您需要手动安装，您可以在终端中分别运行下面的代码来安装：
 
 ```
 pip install aliyun-python-sdk-alinlp
 pip install tqdm
+pip install baidu-aip
+pip install chardet
 ```
+
+在运行之前，请找到脚本（Scripts文件夹下的main.py文件），并修改下面的内容：
+
+### 使用阿里云的情感分析服务配置
+
+如果您没有将**阿里云**App Key和App Secret添加至环境变量，则将下面的内容：
+
+```
+access_key_id = os.environ[`NLP_AK_ENV`]
+access_key_secret = os.environ[`NLP_SK_ENV`]
+```
+
+修改为：
+
+```
+access_key_id = `这里替换为你获得的AppKey`
+access_key_secret = `这里替换为你获得的AppSecret`
+```
+
+### 使用百度云的情感分析配置
+
+请在`Scripts`文件夹下`main.py`文件中找到下面的代码处
+```
+# 这里为百度开放平台填写处
+APP_ID = `输入APP_ID`
+API_KEY = `输入API_KEY`
+SECRET_KEY = `输入SECRET_KEY`
+```
+
+并将等号右侧的部分修改为你获得的对应内容（**需要使用英文单引号包围**）
+
+**请注意** 请不要将AppKey或AppSecret分享给其他任何人或上传至网络
 
 ## 开始使用
 
@@ -40,39 +57,69 @@ pip install tqdm
 ### StartAnalysis函数
 
 `StartAnalysis()`
-函数包含两个参数，一个为传入的CSV文件（包含需要分析的文本内容），另一个为分析结果的输出地址。在代码中以`TestFile.csv`
-为例（包含20条不同情感的文本）。在运行代码时，如无特殊需要，可以直接修改并保存`TestFile.csv`
-的内容并直接运行，分析结果将会被保存至`Results`文件夹下。
+函数包含五个参数，分别为：
+
+ - `input_file`即传入的CSV文件路径和文件名。默认分析文件为`TestFiles`文件夹下的`TestFile.csv`（包含20条不同情感的文本）。在运行代码时，如无特殊需要，可以直接修改并保存`TestFile.csv`的内容并直接运行，分析结果将会被保存至下一个参数指定的地址。
+ - `output_file`即分析结果的输出地址。在代码中默认保存在`Results`文件夹下的`TestResult.csv`文件。如果您不为保存分析结果的文件改名，则每次会覆写之前的内容；
+ - `colnum` 用于指定数据所在传入文件的哪一列，默认为第一列，即int类型的数字1；
+ - `Ali`是否阿里云进行分析，默认为启用（`True`）。如不使用可以修改为`False`；
+ - `Baidu`是否使用百度开放平台进行分析，默认为启用（`True`）。如不使用可以修改为`False`。由于百度的并发限制较严格（QPS为2），使用百度进行分析时会较慢，约0.6s/条
 
 **请注意**
 
 需要分析的文本内容默认放置在一个`utf-8`编码的`CSV`**文件**的**第一列**内(不包含标题)。您也可以修改该函数的`colnum`
 参数来指定获取数据的列。否则程序会报错。
 
-通常情况下，该API的访问限制为50万次/天，并发限制为20。
+通常情况下，该阿里云API的访问限制为50万次/天，并QPS为20。百度的API访问限制为50万次/年，QPS为2
 
 ### SentimentAnalysis函数
 
 `SentimentAnalysis()`
-函数可以用于分析单条文本并获得结果，用于处理批量分析时因网络或其他问题获得的错误结果。通常情况下，如果未能获得正确的结果，将在结果文件的第二列中呈现报错原因或代码。通常情况下，请检查网络连接或文本是否超出了1000个字。如果手动复制粘贴内容仍然获得报错代码，则可以访问[这个页面](https://help.aliyun.com/document_detail/179345.html)
+函数可以用于分析单条文本并获得结果，用于处理批量分析时因网络或其他问题获得的错误结果。通常情况下，如果未能获得正确的结果，将在结果文件的第二列中呈现报错原因或代码。通常情况下，请检查网络连接或文本是否超出了1000个字。如果手动复制粘贴内容仍然获得报错代码，则可以访问[阿里云的这个页面](https://help.aliyun.com/document_detail/179345.html)或[百度的这个页面](https://ai.baidu.com/ai-doc/NLP/tk6z52b9z#%E9%94%99%E8%AF%AF%E7%A0%81)
 查看错误码部分。或者联系*组会群中背景为白色，主体为一直坐着的大象的同学*。
 
 ## 解读结果
 
-我们关注的内容主要集中在下面的内容：
+我们关注的内容主要集中在下面：
 
-- `sentiment`：为判断的情感类型，包含正面、负面、中性三种情况
-- `positive_prob`：句子极性为正面的概率大小，0-1之间，保留4位小数
-- `neutral_prob`：句子极性为中性的概率大小，0-1之间，保留4位小数
-- `negative_prob`：句子极性为负面的概率大小，0-1之间，保留4位小数
-- `RequestId`：唯一请求id，排查问题的依据
+ - `text`：被分析的文本内容
+ - `AliSentiment`： **阿里**分析判断的情感类型，包含正面、负面、中性三种情况
+ - `AliPositive_prob`：句子极性为正面的概率大小，0-1之间，保留4位小数
+ - `AliNeutral_prob`：句子极性为中性的概率大小，0-1之间，保留4位小数
+ - `AliNegative_prob`：句子极性为负面的概率大小，0-1之间，保留4位小数
+ - `AliRequestId`：阿里唯一请求id，排查问题的依据
+ - `BaiduSentiment`：**百度**分析判断的情感类型，包含正向、负向、中性三种情况
+ - `BaiduConfidence`：表示情感分类的置信度
+ - `BaiduPositive_prob`：表示情感属于积极类别的概率
+ - `BaiduNegative_prob`：表示情感属于消极类别的概率
+ - `BaiduLogid`：百度记录id，排查问题的依据
 
 ## 引用
 
 您可以考虑下面的引用格式
+
+## 使用阿里云分析服务
 
 阿里云. (2024, January 24). 自然语言处理NLP. https://ai.aliyun.com/nlp
 
 或
 
 aliyun. (2024, January 24). Natural language processing. https://ai.aliyun.com/nlp
+
+## 使用百度开放平台分析服务
+
+百度AI开放平台. (2024, January 28). 情感倾向分析. https://ai.baidu.com/tech/nlp_apply/sentiment_classify
+
+或
+
+
+Baidu AI Open Platform. (2024, January 28). Sentiment Analysis. https://ai.baidu.com/tech/nlp_apply/sentiment_classify
+
+## 如果您想引用本项目
+
+Xu, W. (2024). SentimentAnalysisWithAPI. (2024). GitHub repository. https://github.com/azanghai/SentimentAnalysisWithAPI/
+
+
+
+
+
--- Scripts/main.py
+++ Scripts/main.py
@@ -9,61 +9,150 @@ from aliyunsdkcore.client import AcsClient
 from aliyunsdkcore.acs_exception.exceptions import ClientException
 from aliyunsdkcore.acs_exception.exceptions import ServerException
 
+from aip import AipNlp
+
 # 这里在使用时将等号右边的内容全部删除，并改为由英文引号""包围的key和secret
+# 这里为阿里云填写处
 access_key_id = os.environ['NLP_AK_ENV']
 access_key_secret = os.environ['NLP_SK_ENV']
-
-# 创建AcsClient实例
-client = AcsClient(
+# 创建AcsClient实例(阿里云)
+clientAli = AcsClient(
     access_key_id,
     access_key_secret,
     "cn-hangzhou"
 )
 
+# 这里为百度开放平台填写处
+APP_ID = '输入APP_ID'
+API_KEY = '输入API_KEY'
+SECRET_KEY = '输入SECRET_KEY'
+# 创建百度实例
+clientBaidu = AipNlp(APP_ID, API_KEY, SECRET_KEY)
+
 
 # 这里的函数可以用于处理单条或者错误
-def SentimentAnalysis(text):
+def SentimentAnalysisAli(text):
     request = GetSaChGeneralRequest.GetSaChGeneralRequest()
     request.set_Text(text)
     request.set_ServiceCode("alinlp")
-    response = client.do_action_with_exception(request)
+    response = clientAli.do_action_with_exception(request)
     resp_obj = json.loads(response)
     return resp_obj
 
 
+def SentimentAnalysisBaidu(text):
+    resp_obj = clientBaidu.sentimentClassify(text.lstrip('\ufeff').encode('gbk').decode('gbk'))
+    return resp_obj
+
+
 # 这里提供了整体处理
-def StartAnalysis(input_file, output_file, colnum=1):
+def StartAnalysis(input_file, output_file, colnum=1, Ali=True, Baidu=True):
     colnum = int(colnum)
     with open(input_file, 'r', encoding='utf-8') as infile, open(output_file, 'w', newline='',
                                                                  encoding='utf-8') as outfile:
         reader = csv.reader(infile)
         writer = csv.writer(outfile)
 
-        # 写入新文件的标题行
-        writer.writerow(['text', 'sentiment', 'positive_prob', 'neutral_prob', 'negative_prob', 'RequestId'])
+        if Ali == True and Baidu == True:
+            # 写入新文件的标题行
+            writer.writerow(
+                ['text', 'AliSentiment', 'AliPositive_prob', 'AliNeutral_prob', 'AliNegative_prob', 'AliRequestId',
+                 'BaiduSentiment', 'BaiduConfidence', 'BaiduPositive_prob', 'BaiduNegative_prob', 'BaiduLogid'])
+            for row in tqdm(reader, desc="Processing"):
+                text = row[colnum - 1]  # 假设文本在第一列
+                resultAli = SentimentAnalysisAli(text)
+                resultBaidu = SentimentAnalysisBaidu(text)
+                # 百度QPS为2,需要缓一下
+                time.sleep(0.6)
+                # 对于阿里进行提取
+                if 'Data' in resultAli:
+                    # 解析 Data 字段
+                    dataAli = json.loads(resultAli['Data'])['result']
+                    updated_row = [text, dataAli['sentiment'], dataAli['positive_prob'], dataAli['neutral_prob'],
+                                   dataAli['negative_prob'], resultAli['RequestId']]
+                else:
+                    # 如果没有 Data 字段，整个返回内容写入第二列
+                    updated_row = [text, json.dumps(resultAli), None, None, None, None]
 
-        for row in tqdm(reader, desc="Processing"):
-            text = row[colnum - 1]  # 假设文本在第一列
-            result = SentimentAnalysis(text)
+                # 对百度进行提取
+                if 'items' in resultBaidu:
+                    # 解析items字段
+                    dataBaidu = resultBaidu['items'][0]
+                    # 替换原数字表述
+                    if dataBaidu['sentiment'] == 1:
+                        dataBaidu['sentiment'] = '中性'
+                    elif dataBaidu['sentiment'] == 0:
+                        dataBaidu['sentiment'] = '负向'
+                    elif dataBaidu['sentiment'] == 2:
+                        dataBaidu['sentiment'] = '正向'
+                    # 拓展行
+                    updated_row.extend([dataBaidu['sentiment'], dataBaidu['confidence'], dataBaidu['positive_prob'],
+                                        dataBaidu['negative_prob'], resultBaidu['log_id']])
+                else:
+                    # 如果没有item字段，则整个返回内容写入第七列
+                    updated_row.extend([json.dumps(resultBaidu), None, None, None, None])
+                # 写入处理后的数据到新文件
+                writer.writerow(updated_row)
+        elif Ali == True and Baidu != True:
+            # 写入新文件的标题行
+            writer.writerow(
+                ['text', 'AliSentiment', 'AliPositive_prob', 'AliNeutral_prob', 'AliNegative_prob', 'AliRequestId'])
+            for row in tqdm(reader, desc="Processing"):
+                text = row[colnum - 1]  # 假设文本在第一列
+                resultAli = SentimentAnalysisAli(text)
+                # 如果提示并发限制则开启下面的代码
+                # time.sleep(0.6)
+                # 对于阿里进行提取
+                if 'Data' in resultAli:
+                    # 解析 Data 字段
+                    dataAli = json.loads(resultAli['Data'])['result']
+                    updated_row = [text, dataAli['sentiment'], dataAli['positive_prob'], dataAli['neutral_prob'],
+                                   dataAli['negative_prob'], resultAli['RequestId']]
+                else:
+                    # 如果没有 Data 字段，整个返回内容写入第二列
+                    updated_row = [text, json.dumps(resultAli), None, None, None, None]
+                # 写入处理后的数据到新文件
+                writer.writerow(updated_row)
+        elif Ali != True and Baidu == True:
+            # 写入新文件的标题行
+            writer.writerow(
+                ['text', 'BaiduSentiment', 'BaiduConfidence', 'BaiduPositive_prob', 'BaiduNegative_prob', 'BaiduLogid'])
+            for row in tqdm(reader, desc="Processing"):
+                text = row[colnum - 1]  # 假设文本在第一列
+                resultBaidu = SentimentAnalysisBaidu(text)
+                # 百度QPS为2,需要缓一下
+                time.sleep(0.6)
+                # 进行占位以对其结果
+                updated_row = [text]
 
-            if 'Data' in result:
-                # 解析 Data 字段
-                data = json.loads(result['Data'])['result']
-                updated_row = [text, data['sentiment'], data['positive_prob'], data['neutral_prob'],
-                               data['negative_prob'], result['RequestId']]
-            else:
-                # 如果没有 Data 字段，整个返回内容写入第二列
-                updated_row = [text, json.dumps(result), None, None, None, None]
-
-            # 写入处理后的数据到新文件
-            writer.writerow(updated_row)
-            # 是如果遇到并发限制的问题，则启用下面的代码
-            # time.sleep(0.1)
+                # 对百度进行提取
+                if 'items' in resultBaidu:
+                    # 解析items字段
+                    dataBaidu = resultBaidu['items'][0]
+                    # 替换原数字表述
+                    if dataBaidu['sentiment'] == 1:
+                        dataBaidu['sentiment'] = '中性'
+                    elif dataBaidu['sentiment'] == 0:
+                        dataBaidu['sentiment'] = '负向'
+                    elif dataBaidu['sentiment'] == 2:
+                        dataBaidu['sentiment'] = '正向'
+                    # 拓展行
+                    updated_row.extend([dataBaidu['sentiment'], dataBaidu['confidence'], dataBaidu['positive_prob'],
+                                        dataBaidu['negative_prob'], resultBaidu['log_id']])
+                else:
+                    # 如果没有item字段，则整个返回内容写入第七列
+                    updated_row.extend([json.dumps(resultBaidu), None, None, None, None])
+                # 写入处理后的数据到新文件
+                writer.writerow(updated_row)
+        else:
+            print('参数输入错误，请至少选择一个分析平台（阿里或百度）')
 
 
 if __name__ == "__main__":
     # 进行整体处理时使用下面这条函数
-    StartAnalysis(input_file='../TestFiles/TestFIle.csv', output_file='../Results/TestResult.csv')
+    StartAnalysis(input_file='../TestFiles/TestFIle.csv', output_file='../Results/TestResult.csv', colnum=1, Ali=True,
+                  Baidu=True)
 
     # 进行单条处理时使用
-    # print(SentimentAnalysis('这里是一个示例'))
+    # print(SentimentAnalysisAli('这里是一个示例'))
+    # print(SentimentAnalysisBaidu('这里是一个示例'))
